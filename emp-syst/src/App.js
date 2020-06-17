b92485dc8a32293562d966ab128af1ad47d1ef42
Manish Verma <31858286+mvermadev@users.noreply.github.com>
2020-06-17 12:53:36 +0530

    Add files via upload

---
+++ emp-syst/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Main from './components/Main';
+
+// Brain of the app. to run it.
+function App() {
+  return (
+    <div>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
